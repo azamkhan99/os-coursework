d6346989f13ed34aa67395d752b6f6a94df3f4c6
azamkhan99 <azam_khan99@outlook.com>
2020-03-02 11:39:09 +0000

    first commits

---
+++ cmake-build-debug/CMakeFiles/deneme.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/coursework/buddy.cpp" "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/cmake-build-debug/CMakeFiles/deneme.dir/coursework/buddy.cpp.o"
+  "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/coursework/cmos-rtc.cpp" "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/cmake-build-debug/CMakeFiles/deneme.dir/coursework/cmos-rtc.cpp.o"
+  "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/coursework/sched-rr.cpp" "/afs/inf.ed.ac.uk/user/s17/s1704037/os-coursework/cmake-build-debug/CMakeFiles/deneme.dir/coursework/sched-rr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../infos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/deneme.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deneme.dir/coursework/buddy.cpp.o"
+  "CMakeFiles/deneme.dir/coursework/cmos-rtc.cpp.o"
+  "CMakeFiles/deneme.dir/coursework/sched-rr.cpp.o"
+  "deneme"
+  "deneme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deneme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
